27b67a60fe2852b0d85e2e08dcc453ec8a350bb8
anuragsingh1409 <90548858+anuragsingh1409@users.noreply.github.com>
2023-10-22 16:17:29 +0530

    final

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const cookieparser = require('cookie-parser');
+app.use(cors());
+app.use(express.json());
+app.use(cookieparser());
+
+var MongoClient = require('mongodb').MongoClient;
+app.get("/",async(req, res)=>{
+    var url = "mongodb+srv://anurag14singh09:Anurag14@cluster0.utyjp3r.mongodb.net/";
+    const client = new MongoClient(url);
+    await client.connect().then(()=>{console.log("COnnected");}).catch(()=>{console.log("Error");});
+    const db = client.db('projects');
+    const collection= db.collection('project_collection');
+  await collection.find({}).toArray().then(e=>res.send(e)).catch(e=>console.log(e));
+  client.close().then(e=>console.log("Closed")).catch(e=>console.log("Error Closing"));
+});
+app.listen(8000, (e)=>{
+    console.log("Listening to the server");
+})
