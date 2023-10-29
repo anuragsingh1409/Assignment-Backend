58cdcb784e848309c26b65a5cea31e5be5546a12
anuragsingh1409 <90548858+anuragsingh1409@users.noreply.github.com>
2023-10-29 11:50:15 +0530

    final1

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const cookieparser = require('cookie-parser');
 app.use(cors());
 app.use(express.json());
 app.use(cookieparser());
+const port = process.env.PORT || 3000;
 
 var MongoClient = require('mongodb').MongoClient;
 app.get("/",async(req, res)=>{
@@ -16,6 +17,6 @@ app.get("/",async(req, res)=>{
   await collection.find({}).toArray().then(e=>res.send(e)).catch(e=>console.log(e));
   client.close().then(e=>console.log("Closed")).catch(e=>console.log("Error Closing"));
 });
-app.listen(8000, (e)=>{
-    console.log("Listening to the server");
-})
+app.listen(port, (e)=>{
+    console.log("Listening to the server on"+port);
+});
